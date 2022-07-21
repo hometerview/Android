b89865101d265de3f79607a9a0eee492252c9ff9
HayleyKim0716 <hl.kim@dramancompany.com>
2022-07-21 22:40:57 +0900

    #[1] feat/로그인 : LoginActivity 추가

---
+++ app/src/main/java/com/ftw/hometerview/ui/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.ftw.hometerview.ui.login
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.ftw.hometerview.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
