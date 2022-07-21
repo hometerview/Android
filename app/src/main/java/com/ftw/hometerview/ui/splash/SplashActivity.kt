f8ec250da104f22d440294bf16adb3a31bffd52d
HayleyKim0716 <hl.kim@dramancompany.com>
2022-07-21 22:38:09 +0900

    #[1] feat/로그인 : SplashActivity 추가

---
+++ app/src/main/java/com/ftw/hometerview/ui/splash/SplashActivity.kt
@@ -0,0 +1,14 @@
+package com.ftw.hometerview.ui.splash
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.ftw.hometerview.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+
+    }
+}
