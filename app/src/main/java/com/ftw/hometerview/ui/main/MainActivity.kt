4993ada26c3e0812f93e84962a522dfbfd78a38b
HayleyKim0716 <hl.kim@dramancompany.com>
2022-07-27 12:24:13 +0900

    #1 refactor/로그인 : `SplashViewModel` 추가

--- app/src/main/java/com/ftw/hometerview/ui/main/MainActivity.kt
+++ app/src/main/java/com/ftw/hometerview/ui/main/MainActivity.kt
@@ -4,14 +4,9 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.ftw.hometerview.R
 
-
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-//        val useCase: LoginUseCase = LoginUseCaseImpl()
-//        Log.d("MainActivity123123", "keyHash: ${Utility.getKeyHash(this)}")
-
     }
 }
--- data/src/main/java/com/ftw/data/repository/login/LoginRepositoryImpl.kt
+++ data/src/main/java/com/ftw/data/repository/login/LoginRepositoryImpl.kt
@@ -4,6 +4,7 @@ import com.ftw.domain.repository.login.LoginRepository
 
 class LoginRepositoryImpl : LoginRepository {
     override fun login(token: String): Result<String> {
+        // TODO: token 으로 login 하는 로직 추가
         return Result.success("temporary token")
     }
 }
