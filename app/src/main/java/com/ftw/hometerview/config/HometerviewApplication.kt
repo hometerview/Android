d71ae932a3183abdbc122be62335e7125f798461
HayleyKim0716 <hl.kim@dramancompany.com>
2022-07-28 20:50:06 +0900

    #1 refactor/로그인 : flipper 추가 - 초기화 로직 구현

--- app/src/main/java/com/ftw/hometerview/config/HometerviewApplication.kt
+++ app/src/main/java/com/ftw/hometerview/config/HometerviewApplication.kt
@@ -1,6 +1,13 @@
 package com.ftw.hometerview.config
 
 import android.app.Application
+import com.facebook.flipper.android.AndroidFlipperClient
+import com.facebook.flipper.android.utils.FlipperUtils
+import com.facebook.flipper.plugins.inspector.DescriptorMapping
+import com.facebook.flipper.plugins.inspector.InspectorFlipperPlugin
+import com.facebook.flipper.plugins.network.NetworkFlipperPlugin
+import com.facebook.soloader.SoLoader
+import com.ftw.hometerview.BuildConfig
 import com.ftw.hometerview.R
 import com.kakao.sdk.common.KakaoSdk
 import dagger.hilt.android.HiltAndroidApp
@@ -10,6 +17,22 @@ class HometerviewApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        initializeKakaoSdk()
+        initializeFlipper()
+    }
+
+    private fun initializeKakaoSdk() {
         KakaoSdk.init(this, getString(R.string.kakao_api_key))
+    }
+
+    private fun initializeFlipper() {
+        SoLoader.init(this, false)
+
+        if (BuildConfig.DEBUG && FlipperUtils.shouldEnableFlipper(this)) {
+            AndroidFlipperClient.getInstance(this).apply {
+                addPlugin(InspectorFlipperPlugin(this@HometerviewApplication, DescriptorMapping.withDefaults()))
+                addPlugin(NetworkFlipperPlugin())
+            }.run { start() }
+        }
     }
 }
