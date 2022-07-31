81767627d76a43cedd1fa1667763ccf94c81fd80
seoggyui <likppi10@gmail.com>
2022-08-01 02:46:52 +0900

    #6 onboarding/온보딩 화면 : 디자인 포맷 설정 📰 - 이전에 구현한 화면에 설정한 디자인 값들 설정 - 상태바 글자색 변경 (검정색으로)

--- app/src/main/java/com/ftw/hometerview/ui/main/MainActivity.kt
+++ app/src/main/java/com/ftw/hometerview/ui/main/MainActivity.kt
@@ -1,17 +1,40 @@
 package com.ftw.hometerview.ui.main
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Parcelable
 import android.util.Log
 import com.ftw.domain.usecase.login.LoginUseCase
 import com.ftw.domain.usecase.login.LoginUseCaseImpl
 import com.ftw.hometerview.R
+import kotlinx.parcelize.Parcelize
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        val ARGUMENT_KEY = "ARGUMENT_KEY"
+        fun newIntent(context: Context, a: String, b: Int): Intent {
+            return Intent(context, MainActivity::class.java)
+                .putExtra(ARGUMENT_KEY, Argument(a, b))
+        }
+    }
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        intent.getParcelableExtra<Argument>(ARGUMENT_KEY)?.let {
 
+        } ?: finish()
 
     }
+
+    @Parcelize
+    private data class Argument(
+        val a: String,
+        val b: Int
+    ) : Parcelable
 }
