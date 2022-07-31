f271d650bd5f07378414b256213c12e9f92e138a
seoggyui <likppi10@gmail.com>
2022-07-31 23:24:32 +0900

    #1 login/로그인 화면 : 구현한 로그인 화면 develop에 merge 📰 - 이번 스코프에서는 카카오 로그인만 구현

--- data/src/main/java/com/ftw/data/network/datasource/LoginRemoteDataSource.kt
+++ data/src/main/java/com/ftw/data/network/datasource/LoginRemoteDataSource.kt
@@ -3,16 +3,8 @@ package com.ftw.data.network.datasource
 import com.ftw.data.datasource.LoginDataSource
 import com.ftw.data.remote.api.LoginAPI
 
-<<<<<<< HEAD
-class LoginRemoteDataSource(private val api: LoginAPI): LoginDataSource {
-    override fun login(token: String): Result<String> {
-        TODO("Not yet implemented")
-    }
-}
-=======
 class LoginRemoteDataSource(api: LoginAPI) : LoginDataSource {
     override fun login(token: String): Result<String> {
         return Result.success("")
     }
 }
->>>>>>> develop
