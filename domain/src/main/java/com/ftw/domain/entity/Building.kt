1e3d42c330c91f0173c4713c38e84ddf6db217df
HayleyKim0716 <hl.kim@dramancompany.com>
2022-08-07 13:33:28 +0900

    #6 feat/건물리뷰상세 : `BuildingReview` 및 BuildingReview 를 얻어오는 UseCase 추가

---
+++ domain/src/main/java/com/ftw/domain/entity/Building.kt
@@ -0,0 +1,25 @@
+package com.ftw.domain.entity
+
+data class Building(
+    val id: Long,
+    val name: String,
+    val type: BuildingType,
+    val address: Address,
+    val salePrice: Price,
+    val charterPrice: Price,
+    val rating: Int,
+    val favorite: Boolean
+) {
+    companion object {
+        val NONE = Building(
+            id = 0,
+            name = "",
+            type = BuildingType.OFFICETEL,
+            address = Address(0, 0, "", ""),
+            salePrice = Price("", ""),
+            charterPrice = Price("", ""),
+            rating = 0,
+            favorite = false
+        )
+    }
+}
