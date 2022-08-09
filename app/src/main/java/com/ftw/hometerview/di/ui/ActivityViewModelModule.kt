e4b52904a8231ef3d22af3e753d8caee1233d134
seoggyui <likppi10@gmail.com>
2022-08-09 19:35:01 +0900

    #6 map_fragment/지역,지하철역,건물 검색 화면 : 리팩토링 🔨  - ActivityViewModelModule - provideSearchCompanyResultViewModel rename

--- app/src/main/java/com/ftw/hometerview/di/ui/ActivityViewModelModule.kt
+++ app/src/main/java/com/ftw/hometerview/di/ui/ActivityViewModelModule.kt
@@ -3,16 +3,16 @@ package com.ftw.hometerview.di.ui
 import com.ftw.domain.usecase.login.LoginUseCase
 import com.ftw.domain.usecase.review.GetLocationReviewsUseCase
 import com.ftw.hometerview.dispatcher.Dispatcher
-import com.ftw.hometerview.ui.searchcompanyresult.OnboardingResultViewModel
 import com.ftw.hometerview.ui.main.MainViewModel
 import com.ftw.hometerview.ui.main.home.review.LocationReviewListViewModel
+import com.ftw.hometerview.ui.searchaddressbuilding.SearchAddressBuildingViewModel
+import com.ftw.hometerview.ui.searchcompanyresult.SearchCompanyResultViewModel
 import com.ftw.hometerview.ui.splash.SplashViewModel
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityComponent
 import dagger.hilt.android.scopes.ActivityScoped
-import dagger.hilt.android.scopes.FragmentScoped
 import javax.inject.Named
 
 @Module
