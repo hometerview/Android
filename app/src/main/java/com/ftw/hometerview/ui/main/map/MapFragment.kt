ca2d18562fd41f37d30a96792f63b4f2abf4e84e
seoggyui <likppi10@gmail.com>
2022-08-10 20:03:05 +0900

    #6 map_fragment/지도 화면 : 지도 화면  - MapViewModel을 MapFragment에서 사용하기 위해 MapView 사용 방식 변경 (inflate 관련 문제 해결)  - viewmodel 사용

--- app/src/main/java/com/ftw/hometerview/ui/main/map/MapFragment.kt
+++ app/src/main/java/com/ftw/hometerview/ui/main/map/MapFragment.kt
@@ -10,8 +10,6 @@ import android.location.Location
 import android.location.LocationManager
 import android.os.Bundle
 import android.util.DisplayMetrics
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.View.*
@@ -19,31 +17,38 @@ import android.view.ViewGroup
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.widget.TextView
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.ftw.domain.entity.BuildingMarker
 import com.ftw.domain.entity.StationMarker
 import com.ftw.hometerview.R
 import com.ftw.hometerview.databinding.FragmentMapBinding
 import com.ftw.hometerview.ui.buildinglist.BuildingListActivity
 import com.ftw.hometerview.ui.searchaddressbuilding.SearchAddressBuildingActivity
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 import net.daum.mf.map.api.MapPOIItem
 import net.daum.mf.map.api.MapPoint
 import net.daum.mf.map.api.MapView
+import javax.inject.Inject
 
-
+@AndroidEntryPoint
 class MapFragment : Fragment(), MapView.POIItemEventListener, MapView.MapViewEventListener {
 
     companion object {
         fun newInstance() = MapFragment()
     }
 
-    private lateinit var viewModel: MapViewModel
+    @Inject
+    lateinit var viewModel: MapViewModel
     private var _binding: FragmentMapBinding? = null
     private val binding get() = _binding!!
     private lateinit var markerRootView: View
     private lateinit var stationTextview: TextView
     private lateinit var cntTextview: TextView
-
+    private lateinit var mapView: MapView
     private var currentZoomLevel: ZoomLevel = ZoomLevel.NONE
 
     override fun onCreateView(
@@ -56,16 +61,18 @@ class MapFragment : Fragment(), MapView.POIItemEventListener, MapView.MapViewEve
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewModel = ViewModelProvider(this)[MapViewModel::class.java]
 
-        binding.mapView.setPOIItemEventListener(this@MapFragment)
-        binding.mapView.setMapViewEventListener(this@MapFragment)
+        mapView = MapView(requireActivity())
+        binding.mapView.addView(mapView)
+
+        mapView.setPOIItemEventListener(this@MapFragment)
+        mapView.setMapViewEventListener(this@MapFragment)
 
         setCustomMarkerView()
         sampleStationMarkerItems()
 
         // 중심점 변경 + 줌 레벨 변경
-        binding.mapView.setMapCenterPointAndZoomLevel(
+        mapView.setMapCenterPointAndZoomLevel(
             MapPoint.mapPointWithGeoCoord(
                 37.50745434356066, 127.03391894910082
             ), 4, true
@@ -80,6 +87,11 @@ class MapFragment : Fragment(), MapView.POIItemEventListener, MapView.MapViewEve
         }
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
     private fun setCustomMarkerView() {
         markerRootView =
             LayoutInflater.from(requireContext()).inflate(R.layout.map_item_station_marker, null)
@@ -89,17 +101,15 @@ class MapFragment : Fragment(), MapView.POIItemEventListener, MapView.MapViewEve
 
     private fun sampleStationMarkerItems() {
         // 이곳에서 통신 : 역에 따른 리스트, MapPointBounds를 보낸다 생각
-        val stationMarkerList: ArrayList<StationMarker> = ArrayList()
-        stationMarkerList.add(StationMarker("강남구청역", 37.51732701554125, 127.04125331828345, 38))
-        stationMarkerList.add(StationMarker("학동역", 37.51450076427542, 127.03188613098999, 24))
-        stationMarkerList.add(StationMarker("선정릉역", 37.51042438956853, 127.04385094188316, 24))
-        stationMarkerList.add(StationMarker("언주역", 37.50745434356066, 127.03391894910082, 105))
-        stationMarkerList.add(StationMarker("신논현역", 37.50468158441123, 127.02446281227596, 1705))
-        stationMarkerList.add(StationMarker("강남역", 37.498157604159395, 127.0276271154474, 67))
-        stationMarkerList.add(StationMarker("역삼역", 37.500750102391144, 127.0364717670947, 13))
-
-        for (stationMarker in stationMarkerList) {
-            addStationMarker(stationMarker)
+        viewModel.setLocation("강남구")
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.marker.collect {
+                    for (stationMarker in it) {
+                        addStationMarker(stationMarker)
+                    }
+                }
+            }
         }
     }
 
@@ -116,7 +126,7 @@ class MapFragment : Fragment(), MapView.POIItemEventListener, MapView.MapViewEve
             MapPoint.mapPointWithGeoCoord(stationMarker.latitude, stationMarker.longitude)
         customMarker.markerType = MapPOIItem.MarkerType.CustomImage // 마커타입을 커스텀 마커로 지정.
 
-        customMarker.customImageBitmap = createDrawableFromView(requireContext(), markerRootView)
+        customMarker.customImageBitmap = createDrawableFromView(requireActivity(), markerRootView)
         customMarker.isCustomImageAutoscale =
             false // hdpi, xhdpi 등 안드로이드 플랫폼의 스케일을 사용할 경우 지도 라이브러리의 스케일 기능을 꺼줌.
 
@@ -125,7 +135,7 @@ class MapFragment : Fragment(), MapView.POIItemEventListener, MapView.MapViewEve
             0.8f
         ) // 마커 이미지중 기준이 되는 위치(앵커포인트) 지정 - 마커 이미지 좌측 상단 기준 x(0.0f ~ 1.0f), y(0.0f ~ 1.0f) 값.
 
-        binding.mapView.addPOIItem(customMarker)
+        mapView.addPOIItem(customMarker)
 
     }
 
@@ -136,17 +146,15 @@ class MapFragment : Fragment(), MapView.POIItemEventListener, MapView.MapViewEve
 
     private fun sampleBuildingMarkerItems() {
         // 이곳에서 통신 : 해당역에 따른 리스트, MapPointBounds를 보낸다 생각
-        val buildingMarkerList: ArrayList<BuildingMarker> = ArrayList()
-        buildingMarkerList.add(BuildingMarker("죽암 빌딩", 37.50236738491151, 127.03654606433355, 1))
-        buildingMarkerList.add(BuildingMarker("동복 빌딩", 37.501057286645356, 127.03363332712635, 2))
-        buildingMarkerList.add(BuildingMarker("정인 빌딩", 37.50065429139799, 127.04018674675399, 3))
-        buildingMarkerList.add(BuildingMarker("창성 빌딩", 37.50095326004147, 127.03504691863799, 4))
-        buildingMarkerList.add(BuildingMarker("진성 빌딩", 37.49987639741427, 127.03558359012032, 5))
-        buildingMarkerList.add(BuildingMarker("근도 빌딩", 37.49864101548692, 127.03877207326005, 6))
-        buildingMarkerList.add(BuildingMarker("화원 빌딩", 37.49728970863099, 127.03814940421368, 7))
-
-        for (buildingMarker in buildingMarkerList) {
-            addBuildingMarker(buildingMarker)
+        viewModel.setBuildingLocation("역삼역")
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.buildingMarker.collect {
+                    for (stationMarker in it) {
+                        addBuildingMarker(stationMarker)
+                    }
+                }
+            }
         }
     }
 
@@ -160,7 +168,7 @@ class MapFragment : Fragment(), MapView.POIItemEventListener, MapView.MapViewEve
             MapPoint.mapPointWithGeoCoord(buildingMarker.latitude, buildingMarker.longitude)
         customMarker.markerType = MapPOIItem.MarkerType.CustomImage // 마커타입을 커스텀 마커로 지정.
 
-        customMarker.customImageBitmap = createDrawableFromView(requireContext(), markerRootView)
+        customMarker.customImageBitmap = createDrawableFromView(requireActivity(), markerRootView)
         customMarker.isCustomImageAutoscale =
             false // hdpi, xhdpi 등 안드로이드 플랫폼의 스케일을 사용할 경우 지도 라이브러리의 스케일 기능을 꺼줌.
 
@@ -169,7 +177,7 @@ class MapFragment : Fragment(), MapView.POIItemEventListener, MapView.MapViewEve
             0.8f
         ) // 마커 이미지중 기준이 되는 위치(앵커포인트) 지정 - 마커 이미지 좌측 상단 기준 x(0.0f ~ 1.0f), y(0.0f ~ 1.0f) 값.
 
-        binding.mapView.addPOIItem(customMarker)
+        mapView.addPOIItem(customMarker)
     }
 
     // View를 Bitmap으로 변환
@@ -248,7 +256,6 @@ class MapFragment : Fragment(), MapView.POIItemEventListener, MapView.MapViewEve
     override fun onMapViewDragEnded(p0: MapView?, p1: MapPoint?) {}
     override fun onMapViewMoveFinished(p0: MapView?, p1: MapPoint?) {}
     override fun onMapViewZoomLevelChanged(p0: MapView, p1: Int) {
-        Log.d("MapFragment", "zoomLevel: ${p0.zoomLevel}")
         if (p0.zoomLevel > 2) {
 
             binding.buildingListButton.apply {
@@ -282,7 +289,7 @@ class MapFragment : Fragment(), MapView.POIItemEventListener, MapView.MapViewEve
 
     @SuppressLint("MissingPermission")
     private fun startTracking() {
-        binding.mapView.currentLocationTrackingMode =
+        mapView.currentLocationTrackingMode =
             MapView.CurrentLocationTrackingMode.TrackingModeOnWithoutHeading  //이 부분
 
         val lm: LocationManager =
@@ -299,7 +306,7 @@ class MapFragment : Fragment(), MapView.POIItemEventListener, MapView.MapViewEve
         marker.mapPoint = uNowPosition
         marker.markerType = MapPOIItem.MarkerType.BluePin
         marker.selectedMarkerType = MapPOIItem.MarkerType.RedPin
-        binding.mapView.addPOIItem(marker)
+        mapView.addPOIItem(marker)
     }
 }
 
