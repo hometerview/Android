ca621dc2d75d76c79a8cb33121404bbd4c872cdf
seoggyui <likppi10@gmail.com>
2022-08-10 19:52:07 +0900

    #6 map_fragment/지도 화면 : 지도 화면  - MapViewModel 추가 및 모듈에 등록

--- app/src/main/java/com/ftw/hometerview/ui/main/map/MapViewModel.kt
+++ app/src/main/java/com/ftw/hometerview/ui/main/map/MapViewModel.kt
@@ -1,7 +1,53 @@
 package com.ftw.hometerview.ui.main.map
 
-import androidx.lifecycle.ViewModel
+import com.ftw.domain.entity.BuildingMarker
+import com.ftw.domain.entity.StationMarker
+import com.ftw.domain.usecase.map.GetBuildingMarkerUseCase
+import com.ftw.domain.usecase.map.GetMarkerUseCase
+import com.ftw.hometerview.dispatcher.Dispatcher
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.*
 
-class MapViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class MapViewModel(
+    dispatcher: Dispatcher,
+    getMarkerUseCase: GetMarkerUseCase,
+    getBuildingMarkerUseCase: GetBuildingMarkerUseCase
+) {
+    private val location: MutableStateFlow<String> = MutableStateFlow("")
+    private val buildingLocation: MutableStateFlow<String> = MutableStateFlow("")
+
+    val marker: StateFlow<List<StationMarker>> = location.filter { it.isNotBlank() }
+        .transformLatest { location ->
+            flow<List<StationMarker>> {
+                emit(
+                    getMarkerUseCase(location)
+                )
+            }
+                .collect {
+                    emit(it)
+                }
+        }
+        .stateIn(CoroutineScope(dispatcher.ui()), SharingStarted.Eagerly, emptyList())
+
+    val buildingMarker: StateFlow<List<BuildingMarker>> = buildingLocation.filter { it.isNotBlank() }
+        .transformLatest { location ->
+            flow<List<BuildingMarker>> {
+                emit(
+                    getBuildingMarkerUseCase(location)
+                )
+            }
+                .collect {
+                    emit(it)
+                }
+        }
+        .stateIn(CoroutineScope(dispatcher.ui()), SharingStarted.Eagerly, emptyList())
+
+
+    fun setLocation(location: String) {
+        this.location.value = location
+    }
+
+    fun setBuildingLocation(buildingLocation: String) {
+        this.buildingLocation.value = buildingLocation
+    }
 }
