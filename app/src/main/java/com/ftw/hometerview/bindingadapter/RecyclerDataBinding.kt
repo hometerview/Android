7e2e440f49e6f948bd7b28122cec3d6539d44456
HayleyKim0716 <hl.kim@dramancompany.com>
2022-08-10 20:21:49 +0900

    #6 fix/홈 : 홈 화면 디자인 적용

--- app/src/main/java/com/ftw/hometerview/bindingadapter/RecyclerDataBinding.kt
+++ app/src/main/java/com/ftw/hometerview/bindingadapter/RecyclerDataBinding.kt
@@ -1,5 +1,6 @@
 package com.ftw.hometerview.adapter
 
+import android.util.Log
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 
@@ -8,6 +9,7 @@ fun setRecyclerViewItems(
     recyclerView: RecyclerView,
     items: List<RecyclerItem>?
 ) {
+    Log.d("RecyclerBindingAdapter", "items: ${items?.size}")
     var adapter = (recyclerView.adapter as? DataBindingRecyclerAdapter)
     if (adapter == null) {
         adapter = DataBindingRecyclerAdapter()
--- app/src/main/java/com/ftw/hometerview/ui/main/home/HomeFragment.kt
+++ app/src/main/java/com/ftw/hometerview/ui/main/home/HomeFragment.kt
@@ -1,9 +1,15 @@
 package com.ftw.hometerview.ui.main.home
 
+import android.animation.ValueAnimator
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AlphaAnimation
+import androidx.core.animation.doOnEnd
+import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
+import androidx.core.view.updateLayoutParams
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Lifecycle
@@ -11,15 +17,18 @@ import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import com.ftw.hometerview.R
 import com.ftw.hometerview.databinding.FragmentHomeBinding
+import com.ftw.hometerview.ui.main.HomeTabItemView
 import com.google.android.material.tabs.TabLayoutMediator
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 import javax.inject.Inject
+import kotlin.math.abs
 
 @AndroidEntryPoint
 class HomeFragment : Fragment() {
 
     companion object {
+        private const val TOOLBAR_LAYOUT_EXPANDING_OFFSET = 100
         fun newInstance() = HomeFragment()
     }
 
@@ -28,6 +37,9 @@ class HomeFragment : Fragment() {
 
     @Inject
     lateinit var viewModel: HomeViewModel
+
+    private lateinit var adapter: HomeViewPagerAdapter
+    private var toolbarLayoutState: ToolbarLayoutState = ToolbarLayoutState.EXPANDING
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -46,6 +58,8 @@ class HomeFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        setOnOffsetChangedListener()
+        startInducementBanner()
         observe()
     }
 
@@ -56,16 +70,87 @@ class HomeFragment : Fragment() {
 
     private fun observe() {
         lifecycleScope.launch {
-            repeatOnLifecycle(Lifecycle.State.STARTED) {
+            repeatOnLifecycle(Lifecycle.State.CREATED) {
                 viewModel.reviews.collect { reviews ->
-                    val activity = activity ?: return@collect
                     val locations = reviews.map { it.location }
-                    binding.viewPager.adapter = HomeViewPagerAdapter(activity, locations)
+                    binding.viewPager.adapter =
+                        HomeViewPagerAdapter(activity ?: return@collect, locations)
                     TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
-                        tab.text = locations[position]
+                        tab.customView = HomeTabItemView(requireContext()).apply {
+                            argument = HomeTabItemView.Argument(
+                                title = locations[position],
+                                showBadge = position == 0
+                            )
+                        }
                     }.attach()
                 }
             }
         }
+    }
+
+    private fun setOnOffsetChangedListener() {
+        binding.appBarLayout.addOnOffsetChangedListener { _, verticalOffset ->
+            val totalScrollRange = binding.appBarLayout.totalScrollRange
+            val absVerticalOffset = abs(verticalOffset)
+
+            if (absVerticalOffset >= totalScrollRange - TOOLBAR_LAYOUT_EXPANDING_OFFSET && toolbarLayoutState != ToolbarLayoutState.COLLAPSED) { // 접혔을 때
+                toolbarLayoutState = ToolbarLayoutState.COLLAPSED
+                binding.tabLayout.background = ContextCompat.getDrawable(
+                    requireContext(),
+                    R.drawable.bg_home_tab_collapsed_item_background
+                )
+                binding.tabLayout.updateLayoutParams<ViewGroup.MarginLayoutParams> {
+                    height = resources.getDimensionPixelSize(R.dimen.home_tab_layout_collapsed_contents_height)
+                }
+
+                for (i in 0 until binding.tabLayout.tabCount) {
+                    (binding.tabLayout.getTabAt(i)?.customView as? HomeTabItemView)?.updateCollapsedBackground()
+                }
+            } else if (absVerticalOffset < totalScrollRange - (TOOLBAR_LAYOUT_EXPANDING_OFFSET * 2) && toolbarLayoutState != ToolbarLayoutState.EXPANDING) { // expanding
+                toolbarLayoutState = ToolbarLayoutState.EXPANDING
+                binding.tabLayout.setBackgroundColor(
+                    ContextCompat.getColor(
+                        requireContext(),
+                        android.R.color.transparent
+                    )
+                )
+                binding.tabLayout.updateLayoutParams<ViewGroup.MarginLayoutParams> {
+                    height = resources.getDimensionPixelSize(R.dimen.home_tab_layout_expanding_contents_height)
+                }
+
+                for (i in 0 until binding.tabLayout.tabCount) {
+                    (binding.tabLayout.getTabAt(i)?.customView as? HomeTabItemView)?.updateExpandedBackground()
+                }
+            }
+        }
+    }
+
+    private fun startInducementBanner() {
+        binding.inducementLayout.isVisible = false
+        ValueAnimator.ofInt(
+            0,
+            resources.getDimensionPixelSize(R.dimen.home_inducement_banner_height)
+        ).apply {
+            addUpdateListener { valueAnimator ->
+                val value = valueAnimator.animatedValue as? Int ?: 0
+                binding.inducementEmptyLayout.updateLayoutParams<ViewGroup.LayoutParams> {
+                    height = value
+                }
+            }
+
+            doOnEnd {
+                binding.inducementLayout.isVisible = true
+                binding.inducementLayout.startAnimation(
+                    AlphaAnimation(0f, 1f).apply {
+                        duration = 1000
+                    }
+                )
+            }
+            duration = 2000
+        }.start()
+    }
+
+    private enum class ToolbarLayoutState {
+        COLLAPSED, EXPANDING
     }
 }
