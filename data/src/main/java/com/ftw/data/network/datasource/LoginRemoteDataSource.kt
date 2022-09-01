e037bf1179dc05ca90a680eae5792d692151af39
HayleyKim0716 <hl.kim@dramancompany.com>
2022-09-01 21:43:37 +0900

    #38 refactor/LoginAPI : 불필요한 코드 제거

--- data/src/main/java/com/ftw/data/network/datasource/LoginRemoteDataSource.kt
+++ data/src/main/java/com/ftw/data/network/datasource/LoginRemoteDataSource.kt
@@ -5,10 +5,6 @@ import com.ftw.data.remote.api.LoginAPI
 
 class LoginRemoteDataSource(private val api: LoginAPI) : LoginDataSource {
     override suspend fun login(token: String): Result<String> {
-        return api.searchUser("hayley", 1, 100)
-            .items
-            .firstOrNull()
-            ?.let { Result.success(it.login) }
-            ?: Result.success("failed")
+        return Result.success("")
     }
 }
--- data/src/main/java/com/ftw/data/remote/api/LoginAPI.kt
+++ data/src/main/java/com/ftw/data/remote/api/LoginAPI.kt
@@ -1,14 +1,3 @@
 package com.ftw.data.remote.api
 
-import com.ftw.data.remote.response.SearchUserResponseData
-import retrofit2.http.GET
-import retrofit2.http.Query
-
-interface LoginAPI {
-    @GET("/search/users")
-    suspend fun searchUser(
-        @Query("q") keyword: String,
-        @Query("page") page: Int,
-        @Query("per_page") perPage: Int
-    ): SearchUserResponseData
-}
+interface LoginAPI
